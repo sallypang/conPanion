5b0e2300bc8bbf1bf438fda4a858c4925e7fa115
Sally Pang <sally.pang@dynamic-leap.com>
2016-04-29 13:12:22 -0700

    Add loginViewController

---
+++ ConPanion/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  ConPanion
+//
+//  Created by Sally Pang on 2016-04-29.
+//  Copyright © 2016 Sally Pang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+}
