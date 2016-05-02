4499c98937691ce9d68b1c741594a79264ed3c45
Sally Pang <sally.pang@dynamic-leap.com>
2016-05-02 09:36:12 -0700

    stared myEvents tab

--- ConPanion/MyConcertsViewController.swift
+++ ConPanion/MyConcertsViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
-class MyConcertsViewController: UIViewController {
+class MyConcertsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var events = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +25,18 @@ class MyConcertsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: TableViewDelegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    */
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.events.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("EventCell", forIndexPath: indexPath)
+        
+        return cell
+    }
 }
